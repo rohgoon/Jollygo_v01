e44bcb2454d4b02aa1c3c721af7c3e9e6fefc31a
NCG <rohgoon@gmail.com>
2017-05-24 18:03:35 +0900

    삭제 기능 오류 해결 (visible관련은 추가로 정리요망)

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/RvAdapter.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/RvAdapter.java
@@ -1,10 +1,14 @@
 package kr.or.dgit.bigdata.jollygo.jollygo_v01;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.DragEvent;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,7 +16,9 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,28 +30,37 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
     private List<String> mDataset;
     private static int lastPosition = -1;
     private Context context;
-    private int longClickDuration = 3000;
-    private boolean isLongPress = false;
-
-
+    private static FloatingActionButton floatingActionButton;
 
     public RvAdapter(List<String> myDataset, Context mContext) {
         mDataset = myDataset;
         context = mContext;
+
     }
 
-
+    public RvAdapter(List<String> mDataset, Context context, FloatingActionButton floatingActionButton) {
+        this.mDataset = mDataset;
+        this.context = context;
+        this.floatingActionButton = floatingActionButton;
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView ivCard;
         TextView tvTest;
         CardView cv;
+        RelativeLayout rl;
+        FloatingActionButton fab; //플로팅버튼 받아오기
+
         public ViewHolder(View v) {
             super(v);
             cv = (CardView) v;
             ivCard = (ImageView) v.findViewById(R.id.ivCard);
             tvTest = (TextView) v.findViewById(R.id.tvTest);
+            rl = (RelativeLayout) v.findViewById(R.id.card_layout);
+            fab = floatingActionButton;
         }
+
+
     }
 
     // Create new views (invoked by the layout manager)
@@ -60,26 +75,57 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {//이벤트 처리
-
 
         setAnimationFadeIn(holder.cv, position);
         holder.ivCard.setImageResource(R.drawable.jg_icon);
         holder.tvTest.setText(mDataset.get(position));
-
-
+        final int[] clickIndex = new int[1];
 
        holder.ivCard.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
-
-                    mDataset.remove(position);
-                    notifyDataSetChanged();
+                    ClipData clip = ClipData.newPlainText("","");
+                    v.startDrag(clip, new View.DragShadowBuilder(v),null, 0);
+                    Toast.makeText(context,position+"번 클릭",Toast.LENGTH_SHORT).show();
+                    clickIndex[0] = position;
+                    holder.fab.setImageResource(R.drawable.ic_delete);
                     return true;
+                   /* mDataset.remove(position);
+
+                    notifyDataSetChanged();
+                    return true;*/
                 }
        });
+        //프롤팅버튼에 드랍 이벤트 주기
+        View.OnDragListener fabDragListener = new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                FloatingActionButton fb = null;
+                if(v instanceof FloatingActionButton){
+                    fb= (FloatingActionButton) v;
+                }
+                switch (event.getAction()){
+                    case DragEvent.ACTION_DROP:
+                        fb.setImageResource(R.drawable.ic_action_name);
+                        mDataset.remove(clickIndex[0]);
+                        notifyItemRemoved(clickIndex[0]);
+                        Toast.makeText(context,clickIndex[0]+"번 삭제",Toast.LENGTH_LONG).show();
+                        notifyDataSetChanged();
+                       // holder.cv.setVisibility(View.VISIBLE);
+                        return true;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        fb.setImageResource(R.drawable.ic_action_name);
+                       // holder.cv.setVisibility(View.VISIBLE);
+                        return true;
+                    default:
+                       // holder.cv.setVisibility(View.VISIBLE);
+                        return true;
+                }
+            }
+        };
+        holder.fab.setOnDragListener(fabDragListener);//드래그 리스너 구현
 
 
     }
@@ -96,6 +142,10 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
         }
 
     }
+
+
+
+
     private void setAnimationFadeIn(View viewToAnimate, int position){
 
         if (position > lastPosition){
@@ -104,6 +154,8 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
             lastPosition = position;
         }
     }
+
+
    /* private void setAnimationFadeOut(View viewToAnimate, int position){
 
         if (position > lastPosition) {
