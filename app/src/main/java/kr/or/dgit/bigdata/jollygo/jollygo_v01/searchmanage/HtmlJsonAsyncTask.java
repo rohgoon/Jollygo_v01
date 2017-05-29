39a19bff23143dbada86675150d6dcf69eea85e3
rohgoon <rohgoon@gmail.com>
2017-05-29 14:03:14 +0900

    Gson 등록 및 htmljson 파싱에 구현, json 요소들 담을 클래스 독립 구현. 단, 테스트는 미구현

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/HtmlJsonAsyncTask.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/HtmlJsonAsyncTask.java
@@ -5,6 +5,9 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.json.JSONArray;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -33,42 +36,34 @@ public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult
         }
         addr +="&hl=ko&noj=1&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiQkuKvicTTAhUBlZQKHXlUC7wQ_AUICigB&biw=853&bih=974";
         List<JSONArray> jsonList = new ArrayList<>();
-        List<SearchResult> srList = new ArrayList<>();
+        List<SearchResult> srList = new ArrayList<>(); // 결과 리스트
         Document doc = null;
         try {
             //json 추출해서 리스트에 삽입
             doc = Jsoup.connect(addr).get();
             Elements jsons = doc.select("div.rg_meta"); //기본 썸네일 thumbimage 작동됨
             for (Element e: jsons) {
+                SearchResult sr = new SearchResult(); //SearchResult 생성
+
                 String jsonStrFirst = e.html();//json 가져옴. 앞뒤로 따옴표 있으니 정리 필요
+                String jsonRes = "["+jsonStrFirst.substring(1,jsonStrFirst.length()-2)+"]"; //따옴표 정리. 확인 요망
+                Log.e("파싱 스트링값 >>>>>>>>>>>>> ", jsonRes);
                 //유니코드 정리 필요 Gson 확인요망
-                /*Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-                String noEscapeStr = gson.toJson(queryCodeList).toString();*/
+                Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
-                String jsonRes = "["+jsonStrFirst.substring(1,jsonStrFirst.length()-2)+"]"; //따옴표 정리
-                JSONArray jarr = new JSONArray(jsonRes);
-                jsonList.add(jarr); //JSONArray 형태로 리스트 삽입
+                sr = gson.fromJson(jsonRes,SearchResult.class); // ou : 이미지 pt : 제목 ru : 링크주소
+                //비트맵 처리
+                Log.e("이미지 결과값 >>>>>>>>>>>>> ", sr.getOu());
+                //경우의 수 추가 요망
+                Bitmap imgBitmap;
+                if (sr.getOu().equals("")){
+                    imgBitmap = null;
+                }else{
+                    imgBitmap = mkBitmap(sr.getOu());
+                }
+                sr.setBitmap(imgBitmap);
+                srList.add(sr); // List<SearchResult>에 삽입
             }
-            //json 분류 및 정리
-            for (JSONArray jarr: jsonList) {
-                // \u003d(=), \u0026(&) 등의 유니코드들을 교체시켜야함
-            }
-
-            //비트맵 처리
-            String img = "https:";
-            Log.e("파싱 결과값 >>>>>>>>>>>>> ",img);
-            //경우의 수 추가 요망
-            //Bitmap imgBitmap = mkBitmap(img);
-            Bitmap imgBitmap;
-            if (img.equals("")){
-                imgBitmap = null;
-            }else{
-                imgBitmap = mkBitmap(img);
-            }
-            //SearchResult 생성
-
-            // List<SearchResult>에 삽입
-
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -76,50 +71,6 @@ public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult
 
         return srList;
     }
-
-    /*@Override
-    protected Map<String,Bitmap> doInBackground(String... params) {
-        //차후 DB 구축시 파싱 예외처리 요망
-        String url = "https://ko.wikipedia.org/wiki/" +params[0];
-        Map<String,Bitmap> resMap= new HashMap<>();
-        String img="";
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).get();
-            Element img1 = doc.select("img.thumbimage").first(); //기본 썸네일 thumbimage 작동됨
-            Element img2 = doc.select("li.gallerybox img").first(); //예비1 썸네일 thumbimage 작동됨
-            Element img3 = doc.select("table.infobox a img").first();//예비2 썸네일 thumbimage
-            if(img1!=null&&img2!=null){
-                img = "https:"+img2.attr("src"); // img2에서 src 빼내기
-                Log.e("파싱 출처2 >>>>>>>>>>>>> ",img);
-            }else if(img1 != null&&img2==null){
-                img = "https:"+img1.attr("src"); // img1에서 src 빼내기
-                Log.e("파싱 출처1 >>>>>>>>>>>>> ",img);
-            }else if(img1==null&&img2 !=null){
-                img = "https:"+img2.attr("src"); // img2에서 src 빼내기
-                Log.e("파싱 출처2 >>>>>>>>>>>>> ",img);
-            }else if(img1==null&&img2==null&&img3!=null){
-                img = "https:"+img3.attr("src"); // img3에서 src 빼내기
-                Log.e("파싱 출처3 >>>>>>>>>>>>> ",img);
-            }
-            //img = "https:"+img1.attr("src");
-            Log.e("파싱 결과값 >>>>>>>>>>>>> ",img);
-            //경우의 수 추가 요망
-            //Bitmap imgBitmap = mkBitmap(img);
-            Bitmap imgBitmap;
-            if (img.equals("")){
-                imgBitmap = null;
-            }else{
-                imgBitmap = mkBitmap(img);
-            }
-            resMap.put(params[0],imgBitmap);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return resMap;
-    }*/
     private Bitmap mkBitmap(String s) {
         HttpURLConnection connection = null;
         try {
@@ -138,7 +89,4 @@ public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult
             }
         }
     }
-
-
-
 }
--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/SearchResult.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/SearchResult.java
@@ -8,26 +8,48 @@ import java.util.List;
  * Created by rohgoon on 2017-05-27.
  */
 
-public class SearchResult {
-    private String title;
-    private String addr;
+public class SearchResult {// ou : 이미지 pt : 제목 ru : 링크주소
+    private String ou;
+    private String pt;
+    private String ru;
     private Bitmap bitmap;
 
-    public SearchResult(String title, String addr, Bitmap bitmap) {
-        this.title = title;
-        this.addr = addr;
-        this.bitmap = bitmap;
+    public String getOu() {
+        return ou;
     }
 
-    public String getTitle() {
-        return title;
+    public void setOu(String ou) {
+        this.ou = ou;
     }
 
-    public String getAddr() {
-        return addr;
+    public String getPt() {
+        return pt;
+    }
+
+    public void setPt(String pt) {
+        this.pt = pt;
+    }
+
+    public String getRu() {
+        return ru;
+    }
+
+    public void setRu(String ru) {
+        this.ru = ru;
     }
 
     public Bitmap getBitmap() {
         return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    @Override
+    public String toString() {
+        return "ou:" + ou +
+                "\npt:" + pt +
+                "\nru:" + ru;
     }
 }
