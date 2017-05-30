18cfeb1ae62e0a9dfd9efd6927c443460ad01f86
rohgoon <rohgoon@gmail.com>
2017-05-30 16:38:08 +0900

    리스트 결과까지 파싱 및 출력 완료

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/HtmlJsonAsyncTask.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/searchmanage/HtmlJsonAsyncTask.java
@@ -30,11 +30,12 @@ import java.util.Map;
 public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult>> {
     @Override
     protected List<SearchResult> doInBackground(String... params) {
-        String addr = "www.google.co.kr/search?q=allintext:";
+        String addr = "https://www.google.co.kr/search?q=allintext:+레시피";
         for (String str: params) {
             addr += "+"+str;
         }
-        addr +="&hl=ko&noj=1&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiQkuKvicTTAhUBlZQKHXlUC7wQ_AUICigB&biw=853&bih=974";
+        addr +="+이웃추가&hl=ko&noj=1&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiQkuKvicTTAhUBlZQKHXlUC7wQ_AUICigB&biw=853&bih=974";
+        Log.e("인풋 스트링값 >>>>>>>>>>>>> ", addr);
         List<JSONArray> jsonList = new ArrayList<>();
         List<SearchResult> srList = new ArrayList<>(); // 결과 리스트
         Document doc = null;
@@ -42,11 +43,13 @@ public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult
             //json 추출해서 리스트에 삽입
             doc = Jsoup.connect(addr).get();
             Elements jsons = doc.select("div.rg_meta"); //기본 썸네일 thumbimage 작동됨
+            int cutIndex = 0;
             for (Element e: jsons) {
                 SearchResult sr = new SearchResult(); //SearchResult 생성
 
-                String jsonStrFirst = e.html();//json 가져옴. 앞뒤로 따옴표 있으니 정리 필요
-                String jsonRes = "["+jsonStrFirst.substring(1,jsonStrFirst.length()-2)+"]"; //따옴표 정리. 확인 요망
+                String jsonRes = e.html();//json 가져옴. 그냥 긁어도 기능함.
+
+                //String jsonRes = "["+jsonStrFirst.substring(1,jsonStrFirst.length()-2)+"]"; //따옴표 정리. 확인 요망
                 Log.e("파싱 스트링값 >>>>>>>>>>>>> ", jsonRes);
                 //유니코드 정리 필요 Gson 확인요망
                 Gson gson = new GsonBuilder().disableHtmlEscaping().create();
@@ -63,6 +66,10 @@ public class HtmlJsonAsyncTask extends AsyncTask<String,String,List<SearchResult
                 }
                 sr.setBitmap(imgBitmap);
                 srList.add(sr); // List<SearchResult>에 삽입
+                cutIndex++;
+                if (cutIndex >20){
+                    break;
+                }
             }
 
         } catch (Exception e) {
