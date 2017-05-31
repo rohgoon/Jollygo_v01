fa69fb67bba34437c15f3b04c677cb94144a5e9b
rohgoon <rohgoon@gmail.com>
2017-05-31 15:38:28 +0900

    웹뷰 퍼포먼스 향상 및 웹뷰플로팅아이콘 변경, 클릭시 새창뜨는 기능 없앰. 다음으로 프로그래스바 구현 요망

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/WebActivity.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/WebActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,7 +16,7 @@ import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
     private WebView webView;
-   // private WebSettings webSettings;
+    private WebSettings webSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +28,23 @@ public class WebActivity extends AppCompatActivity {
         //Uri uri = Uri.parse(uriStr);
         webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        //webSettings = webView.getSettings();
+        webSettings = webView.getSettings();
+        //webView.setInitialScale(1);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUserAgentString("Android");
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
         Log.e("웹뷰 주소>>>>>>",uriStr);
         webView.loadUrl(uriStr);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
+
         //로딩에 많은 시간이 걸려 프로그래스바 설치 필요
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabWeb);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {//사진찍기, 뒤로가기(리스트로), 홈으로, 인스타그램에 바로 공유, 브라우저로 열기
             @Override
             public void onClick(View view) {
 
