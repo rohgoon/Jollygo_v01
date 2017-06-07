98c67125bd6b99b44c7359a0b4ecf6a2a51a9b56
rohgoon <rohgoon@gmail.com>
2017-06-07 16:21:25 +0900

    로그인 됨. 로그아웃 테스트 시작

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/MainFrontActivity.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/MainFrontActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
@@ -24,20 +26,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainFrontActivity extends AppCompatActivity {
+public class MainFrontActivity extends AppCompatActivity  implements
+        GoogleApiClient.OnConnectionFailedListener{
     private FirebaseAuth mAuth;
     private GoogleSignInOptions gso;
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 9001;
     private ImageButton ibOauth;
+    private ImageView ivFront;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_front);
         ibOauth= (ImageButton) findViewById(R.id.ibOauth);
-
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.android_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         /*GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -54,27 +57,38 @@ public class MainFrontActivity extends AppCompatActivity {
             public void onClick(View v) {
                 ibOauth.setImageResource(R.drawable.oauthbtn_ns);
                 signIn();
-                /*Intent intent = new Intent(MainFrontActivity.this,SearchActivity.class);
+              /*Intent intent = new Intent(MainFrontActivity.this,SearchActivity.class);
+
                 startActivityForResult(intent,0);*/
             }
         });
 
         mAuth = FirebaseAuth.getInstance();
 
+        ivFront = (ImageView) findViewById(R.id.ivFront);
+        ivFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                updateUI(currentUser);
+            }
+        });
     }//onCreate
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+       // FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        mGoogleApiClient.connect();
     }
 
+    // [START onactivityresult]
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -88,36 +102,42 @@ public class MainFrontActivity extends AppCompatActivity {
                 firebaseAuthWithGoogle(account);
             } else {
                 // Google Sign In failed, update UI appropriately
-                // ...
+                // [START_EXCLUDE]
+                updateUI(null);
+                // [END_EXCLUDE]
             }
         }
-    }//onActivityResult
+    }
+    // [END onactivityresult]
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {//GoogleSignInAccount 개체에서 ID 토큰을 가져와서 Firebase 사용자 인증 정보로 교환하고 Firebase 사용자 인증 정보를 사용해 Firebase에 인증합니다.
-        Log.e("로그인정보", "firebaseAuthWithGoogle:" + acct.getDisplayName());
-        //getDisplayName 유저이름 받아옴
-        //getUid
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("firebaseAuthWithGoogle", "firebaseAuthWithGoogle:" + acct.getId());
+
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("isSuccessful", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("isNotSuccessful", "signInWithCredential:failure", task.getException());
                             Toast.makeText(MainFrontActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
-                        // ...
+
                     }
                 });
     }//firebaseAuthWithGoogle
   private void updateUI(FirebaseUser user) {
         if (user != null) {
-            Toast.makeText(getApplicationContext(),user.getDisplayName(),Toast.LENGTH_LONG).show();
+           // Toast.makeText(getApplicationContext(),user.getDisplayName(),Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(getApplicationContext(),"유저가 없습니다.",Toast.LENGTH_LONG).show();
+           // Toast.makeText(getApplicationContext(),"유저가 없습니다.",Toast.LENGTH_LONG).show();
         }
     }
   private void signOut() {
@@ -134,9 +154,22 @@ public class MainFrontActivity extends AppCompatActivity {
               });
   }
 
- /*   @Override
+   /* @Override
     public void startActivityForResult(Intent intent, int requestCode) {
         super.startActivityForResult(intent, requestCode);
-        ibOauth.setImageResource(R.drawable.oauthbtn02);
+        ibOauth.setImageResource();
     }*/
+   @Override
+   public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+       // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+       // be available.
+       Log.d("커넥팅 실패", "onConnectionFailed:" + connectionResult);
+       Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+   }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
 }
