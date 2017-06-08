95c8055b138dfe46906eb9796ca511f40c10747f
rohgoon <rohgoon@gmail.com>
2017-06-08 18:49:30 +0900

    프래그먼트 틀 완성

--- app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/fragments/FavlinkFragment.java
+++ app/src/main/java/kr/or/dgit/bigdata/jollygo/jollygo_v01/fragments/FavlinkFragment.java
@@ -3,47 +3,38 @@ package kr.or.dgit.bigdata.jollygo.jollygo_v01.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import kr.or.dgit.bigdata.jollygo.jollygo_v01.R;
+import kr.or.dgit.bigdata.jollygo.jollygo_v01.customviews.GridRecyclerView;
 
 public class FavlinkFragment extends Fragment {
-
+    private GridRecyclerView grv;
+    private static ProgressBar bar;
 
 
     public FavlinkFragment() {
         // Required empty public constructor
     }
 
-
-    public static FavlinkFragment newInstance(String param1, String param2) {
-        FavlinkFragment fragment = new FavlinkFragment();
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favlink, container, false);
+        View root = inflater.inflate(R.layout.fragment_favlink,container,false);
+        grv = (GridRecyclerView) root.findViewById(R.id.flRecyclerView);// 확인요망 -> 작동완료
+        bar = (ProgressBar) root.findViewById(R.id.flCardPb);
+        return root;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //뷰 구현
+        
     }
-
-
 }
